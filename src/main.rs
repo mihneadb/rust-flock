a6459761165a6dc7f8d5a6de301231b5d42d3301
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-11-05 22:34:53 +0200

    Update main.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod vector3;
 
 fn main() {
     let v = vector3::Vector3 { x: 1f32, y: 2f32, z: 3f32 };
-    println!("Hello, world! {}", v);
-    println!("Hello, world! {}", v.magnitude());
+    let w = vector3::Vector3 { x: 3f32, y: 2f32, z: 1f32 };
+    println!("{}", v.magnitude());
+    println!("{}", v + w);
 }
