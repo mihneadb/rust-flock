2201b01b07e4c3e174f41fcf0331cef0e98045d6
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-11-05 22:04:25 +0200

    Use vector module in main.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod vector3;
+
 fn main() {
-    println!("Hello, world!")
+    let v = vector3::Vector3 { x: 1f32, y: 2f32, z: 3f32 };
+    println!("Hello, world! {}", v);
+    println!("Hello, world! {}", v.magnitude());
 }
