aae1d946d18a22d2b5918cd51e9121c4a8b10422
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-11-15 01:26:39 +0200

    Use point without namespacing.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use point::Point;
 mod point;
 
 fn main() {
-    let v = point::Point { x: 1i, y: 2i, z: 3i };
-    let w = point::Point { x: 3i, y: 2i, z: 1i };
+    let v = Point { x: 1i, y: 2i, z: 3i };
+    let w = Point { x: 3i, y: 2i, z: 1i };
     println!("{}", v + w);
 }
